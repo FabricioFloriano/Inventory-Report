8314c0d425c73eb9c3bb2f0cbb144c28dbc477cf
Fabricio Floriano <f.florian0@hotmail.com>
2023-11-23 04:56:37 -0300

    Requisito 03-04-08? - Crie a Interface Importer - Crie a classe JsonImporter - Crie a classe CsvImporter

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,16 +1,49 @@
+from inventory_report.product import Product
+import abc
+import json
+import csv
+
 from typing import Dict, Type
 
 
-class Importer:
-    pass
+class Importer(abc.ABC):
+    def __init__(self, path: str) -> None:
+        super().__init__()
+        self.path = path
+        self._import_data: list[Product] = []
+
+    @abc.abstractmethod
+    def import_data(self) -> list[Product]:
+        return self._import_data
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> list[Product]:
+        with open(self.path, "r") as file:
+            self.data = [Product(**product) for product in json.load(file)]
+
+        return self.data
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> list[Product]:
+        with open(self.path, encoding="utf-8") as file:
+            reader_csv = csv.reader(file, delimiter=",", quotechar='"')
+            _, *data = reader_csv
+            self.data = [
+                Product(
+                    id=product[0],
+                    product_name=product[1],
+                    company_name=product[2],
+                    manufacturing_date=product[3],
+                    expiration_date=product[4],
+                    serial_number=product[5],
+                    storage_instructions=product[6],
+                )
+                for product in data
+            ]
+
+        return self.data
 
 
 # Não altere a variável abaixo
