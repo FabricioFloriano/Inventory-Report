060852cb27a095759f3cf31d30f6f7ba4955ce0e
Fabricio Floriano <f.florian0@hotmail.com>
2023-11-23 03:36:27 -0300

    Requisito 01? - Teste o construtor/inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    products = Product(
+        "1",
+        "NoProduct",
+        "NoName",
+        "01/11/2023",
+        "30/11/2024",
+        "12345",
+        "NoInstructions",
+    )
+
+    assert products.id == "1"
+    assert products.company_name == "NoName"
+    assert products.product_name == "NoProduct"
+    assert products.manufacturing_date == "01/11/2023"
+    assert products.expiration_date == "30/11/2024"
+    assert products.serial_number == "12345"
+    assert products.storage_instructions == "NoInstructions"
