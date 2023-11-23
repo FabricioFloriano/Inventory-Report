6c946837cb77173956c25fb69ccaea4e1efc86da
Fabricio Floriano <f.florian0@hotmail.com>
2023-11-23 08:38:27 -0300

    Requisito 07-09 -Criado o relatório SimpleReport -Criado o relatório CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,22 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def generate(self) -> str:
+        report = super().generate()
+
+        get_products = []
+        for inventory in self.inventories:
+            get_products.extend(inventory.data)
+
+        get_by_company = {}
+        for product in get_products:
+            get_by_company[product.company_name] = (
+                get_by_company.get(product.company_name, 0) + 1
+            )
+
+        stocked_products_by_company = "\nStocked products by company:\n"
+        for company, stocked in get_by_company.items():
+            stocked_products_by_company += f"- {company}: {stocked}\n"
+
+        return f"{report}{stocked_products_by_company}"
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,51 @@
-class SimpleReport:
-    pass
+from inventory_report.reports.report import Report
+from inventory_report.inventory import Inventory
+from datetime import datetime
+from collections import Counter
+
+
+class SimpleReport(Report):
+    def __init__(self):
+        self.inventories = []
+
+    def add_inventory(self, inventory: Inventory):
+        self.inventories.append(inventory)
+
+    def generate(self) -> str:
+        all_products = []
+        for inventory in self.inventories:
+            all_products.extend(inventory.data)
+
+        if not all_products:
+            return "No products in inventory."
+
+        manufacturing_dates = [
+            datetime.strptime(product.manufacturing_date, "%Y-%m-%d")
+            for product in all_products
+        ]
+        oldest_manufacturing_date = min(manufacturing_dates).strftime(
+            "%Y-%m-%d"
+        )
+        valid_products = [
+            product
+            for product in all_products
+            if datetime.strptime(product.expiration_date, "%Y-%m-%d")
+            > datetime.now()
+        ]
+        closest_expiration_date = min(
+            valid_products,
+            key=lambda x: datetime.strptime(x.expiration_date, "%Y-%m-%d"),
+        ).expiration_date
+
+        company_counter = Counter(
+            product.company_name for product in all_products
+        )
+        company_with_largest_inventory = company_counter.most_common(1)[0][0]
+
+        report = (
+            f"Oldest manufacturing date: {oldest_manufacturing_date}\n"
+            f"Closest expiration date: {closest_expiration_date}\n"
+            f"Company with the largest inventory: "
+            f"{company_with_largest_inventory}"
+        )
+        return report
