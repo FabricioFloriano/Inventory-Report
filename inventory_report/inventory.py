b87f3c9928847c7d9ff191d219016bc7440841f5
Fabricio Floriano <f.florian0@hotmail.com>
2023-11-23 06:23:42 -0300

    Requisito 05? - Crie a classe Inventory

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,17 @@
+from typing import List, Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None) -> None:
+        if data is not None:
+            self.__data = data
+        else:
+            self.__data = []
+
+    @property
+    def data(self) -> List[Product]:
+        return self.__data
+
+    def add_data(self, data: List[Product]) -> None:
+        self.__data.extend(data)
